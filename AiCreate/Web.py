7a2fba087b2baba437b39190e4c51ca75d939549
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2025-03-13 22:49:24 +0700

    Update Web.py

--- AiCreate/Web.py
+++ AiCreate/Web.py
@@ -1,5 +1,9 @@
-import streamlit as st 
-import pandas as pd
+#ติดตั้งก่อนในเทอมินอล  pip install pandas streamlit
+
+
+import streamlit as st #นำเข้าตัวทำเว็ป
+import pandas as pd #นำเข้าตัววิเคราะห์ข้อมูล
+
 
 
 st.write("hi   my name is Phubet")
