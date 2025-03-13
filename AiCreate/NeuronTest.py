865de8a1c4ca1a0b6a0624559aca2d30a3f9f527
Phubet66040 <s6604062636569@email.kmutnb.ac.th>
2025-03-13 23:15:55 +0700

    add readme

--- AiCreate/NeuronTest.py
+++ AiCreate/NeuronTest.py
@@ -1,4 +1,8 @@
-
+#ติดตั้งก่อนในเทอมินอล pip install tensorflow
 
 import tensorflow as tf
 from tensorflow import keras
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
