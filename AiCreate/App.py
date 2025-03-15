4fd628835fa0af6cf3d0493949cea8edec06c7f3
xobazjr <MR.TeerapatKotanartChannel@Gmail.COM>
2025-03-15 22:31:59 +0700

    Update new page Development Approach
    ✅ Update Development Approach page

--- AiCreate/App.py
+++ AiCreate/App.py
@@ -5,13 +5,16 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import Web
 import machinelearning
+import Detail
 
 import streamlit as st
 
 st.sidebar.title("Navigation")
-page = st.sidebar.selectbox("เลือกหน้า", ["Home", "Machine Learning"])
+page = st.sidebar.selectbox("เลือกหน้า", ["Home", "Development Approach", "Machine Learning"])
 
 if page == "Home":
     Web.show()  # เรียกใช้ฟังก์ชันแสดงหน้า Web
+elif page == "Development Approach":
+    Detail.show()  # เรียกใช้ฟังก์ชันแสดงหน้า Development Approach
 elif page == "Machine Learning":
     machinelearning.show()  # เรียกใช้ฟังก์ชันแสดงหน้า Machine Learning
